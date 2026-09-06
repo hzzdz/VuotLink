1278914b8b58246f35d36d0b981475fd7e3c07e6
hzzdz <duchiep2026qb@gmail.com>
2026-09-06 10:06:16 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+window.GETKEY_CONFIG={
+  api:"https://getkey-api.2mcnfmhghn.workers.dev",
+
+  // Thêm ảnh logo sau:
+  logo:"IMG_0669.jpeg",
+
+  // Điền link thật của bạn sau:
+  telegramAdmin:"#",
+  discordAdmin:"#",
+  telegramGroup:"#",
+  discordGroup:"#"
+};
